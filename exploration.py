660ce179ebe9488b9937e36e3538b221c24a0408
LP <louisphilippe.robichaud.protic@gmail.com>
2026-02-04 20:38:45 -0500

    Converted the strategies to dict

--- exploration.py
+++ exploration.py
@@ -3,6 +3,7 @@ from itertools import product
 from typing import Any
 from typing import Callable
 from typing import Iterable, Tuple, Hashable, List
+import itertools
 
 def pprint_input_output(strat: tuple[tuple[Any], tuple[Any]]):
     """
@@ -29,34 +30,31 @@ def W(x,y,a,b):
         return 0
 
 
+def strategy_tuple_to_dict(strategy_tuple):
+    """
+    Converts a deterministic strategy tuple into a dictionary:
+    (x1, ..., xn) -> (a1, ..., an)
+    """
+    # Build per-player input -> output maps
+    player_maps = []
+    for player in strategy_tuple:
+        player_maps.append(dict(player))
+
+    # Assume all players share the same input alphabet
+    inputs = list(player_maps[0].keys())
+
+    strategy = {}
+    for joint_input in itertools.product(inputs, repeat=len(player_maps)):
+        joint_output = tuple(
+            player_maps[i][joint_input[i]]
+            for i in range(len(player_maps))
+        )
+        strategy[joint_input] = joint_output
+
+    return strategy
+
 view_for_one_player = list(product(X, A)) # list of possible input-output pairs for one bit
 #I want a list that looks like this:
-"""
-[
-    X = [0, 1]
-    A = [0, 1]
-    ((0, 0), (1, 0))
-    ((0, 0), (1, 1))
-    ((0, 1), (1, 0))
-    ((0, 1), (1, 1))
-    X = [0, 1, 2]
-    A = [0, 1]
-    ((0, 0), (1, 0), (2, 0))
-    ((0, 0), (1, 0), (2, 1))
-    ((0, 0), (1, 1), (2, 0))
-    ((0, 0), (1, 1), (2, 1))
-    ((0, 1), (1, 0), (2, 0))
-    ((0, 1), (1, 0), (2, 1))
-    ((0, 1), (1, 1), (2, 0))
-    ((0, 1), (1, 1), (2, 1))
-    
-]
-
-the length of the subset is the number of elements in the input set (len(X))
-There will be len(A)^len(X) subsets -> len(A) * lan(A) * ... * len(A)
-Each of these subsets is a strategy.
-Cartesian product of A, repeated len(X) times -> zipped with X
-"""
 
 #at last, that's it.
 X_t = Hashable
@@ -70,55 +68,15 @@ def all_strategies(X: Iterable[X_t], A: Iterable[A_t]) -> List[Strategy]:
 
 strats = all_strategies(X, A)
 n_players_strats = list(product(strats, repeat=n_players))
-print(len(n_players_strats))
-print(n_players_strats)
 
-#now, this looks like this:
-"""
-[
-    ((strategy for player 1), (strategy for player 2))
-]
-and strategies look liks ((),()), with one sub-tuple for each input in X
-The sub tuples are input-output pairs
-"""
-#let's turn the strategies in dicts, just to make it a bit more reader friendly
-"""
-I would like each strategy to look like this:
-strategy = {
-    (0, 0) -> (output)
-    (0, 1) -> (output)
-    (1, 0) -> (output)
-    (1, 1) -> (output)
-}
-So that we can see exactly how the strategy behaves
-"""
 dict_strats = []
 for strat in n_players_strats:
     # how many tuples will there be? -> n_players , each with |X| tuples
     # strat is smtg like this: (((0, 0), (1, 0)), ((0, 0), (1, 0)))
     #this is behavior data. I must generate all possible questions, then find the behavior
-    dict_strat = {}
-    #just trying github things
-    pass
-# FINISH THIS   
+    dict_strat = strategy_tuple_to_dict(strat)
+    dict_strats.append(dict_strat)
 
-# for strat in n_players_strats:
-#     #how many tuples will there be? -> n_players , each with |X| tuples
-#     dict_strat = {}
-#     for i, x in enumerate(X):
-#         question_answer = []
-#         for player_tup in strat:
-#             question_answer.append(player_tup[i])
-#         #now, question_answer = [(0, 0), (1, 0)], map that
-#         # to (0, 1): (0, 0)
-#         question = []
-#         answer = []
-#         for j in range(len(X)):
-#             question.append(question_answer[j][0])
-#             answer.append(question_answer[j][1])
-#         dict_strat[tuple(question)] = tuple(answer)
-        
-#     dict_strats.append(dict_strat)
     
 for s in dict_strats:
     print(s)
@@ -138,4 +96,4 @@ def calculate_chsh_exclusion(strategy: dict, W: Callable):
     return exclusion_set
 
 all_exclusion_sets = [calculate_chsh_exclusion(s, W) for s in dict_strats]
-print(all_exclusion_sets)
+# print(all_exclusion_sets)
